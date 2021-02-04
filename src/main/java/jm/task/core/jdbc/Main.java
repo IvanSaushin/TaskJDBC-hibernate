392714e5b8ee392ba737119798f11303a74d8b75
Ivan Saushin <ivan.saushin@gmail.com>
2021-02-04 17:42:05 +0400

    add hibernate test in Main class

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
@@ -11,42 +12,61 @@ public class Main {
 
 
     public static void main(String[] args) throws SQLException {
-        List<User> users;
-        UserService userDao= new UserServiceImpl();
 
-        // my test
-        userDao.dropUsersTable();
+        // Hibernate
+        UserDaoHibernateImpl idhi = new UserDaoHibernateImpl();
 
-        userDao.createUsersTable();
-        userDao.saveUser("James", "Smith", (byte) 35);
-        userDao.saveUser("John", "Travolta", (byte) 46);
-        userDao.saveUser("Ms", "Smith", (byte) 32);
-        users = userDao.getAllUsers();
-        users.forEach(System.out::println); // 3 user
-        System.out.println();
+        idhi.dropUsersTable();
+        idhi.createUsersTable();
+        User user1 = new User("alex", "navalny", (byte)34);
+        User user2 = new User("vacya", "guba", (byte)19);
+        idhi.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        idhi.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
 
-        userDao.dropUsersTable();
+        idhi.removeUserById(1);
+//        idhi.cleanUsersTable();
+        List<User> list = idhi.getAllUsers();
+        list.forEach(System.out::println);
 
-        userDao.createUsersTable();
-        userDao.saveUser("Ms", "Smith", (byte) 32);
-        users = userDao.getAllUsers();
-        users.forEach(System.out::println); // 1 user
-        System.out.println();
 
-        userDao.cleanUsersTable();
-        users = userDao.getAllUsers();
-        users.forEach(System.out::println); // // ________
-        System.out.println();
 
-        userDao.saveUser("Ms", "Smith", (byte) 32);
-        userDao.saveUser("John", "Travolta", (byte) 46);
-        users = userDao.getAllUsers();
-        users.forEach(System.out::println); // 2 user
-        System.out.println();
-
-        userDao.removeUserById(1);
-        users = userDao.getAllUsers();
-        users.forEach(System.out::println); // 1 user
+        //JDBC
+//        List<User> users;
+//        UserService userDao= new UserServiceImpl();
+//
+//        // my test
+//        userDao.dropUsersTable();
+//
+//        userDao.createUsersTable();
+//        userDao.saveUser("James", "Smith", (byte) 35);
+//        userDao.saveUser("John", "Travolta", (byte) 46);
+//        userDao.saveUser("Ms", "Smith", (byte) 32);
+//        users = userDao.getAllUsers();
+//        users.forEach(System.out::println); // 3 user
+//        System.out.println();
+//
+//        userDao.dropUsersTable();
+//
+//        userDao.createUsersTable();
+//        userDao.saveUser("Ms", "Smith", (byte) 32);
+//        users = userDao.getAllUsers();
+//        users.forEach(System.out::println); // 1 user
+//        System.out.println();
+//
+//        userDao.cleanUsersTable();
+//        users = userDao.getAllUsers();
+//        users.forEach(System.out::println); // // ________
+//        System.out.println();
+//
+//        userDao.saveUser("Ms", "Smith", (byte) 32);
+//        userDao.saveUser("John", "Travolta", (byte) 46);
+//        users = userDao.getAllUsers();
+//        users.forEach(System.out::println); // 2 user
+//        System.out.println();
+//
+//        userDao.removeUserById(1);
+//        users = userDao.getAllUsers();
+//        users.forEach(System.out::println); // 1 user
 
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -32,7 +32,7 @@ public class Util {
 
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                settings.put(Environment.HBM2DDL_AUTO, "update");
+                settings.put(Environment.HBM2DDL_AUTO, "");
 
                 configuration.setProperties(settings);
                 configuration.addAnnotatedClass(User.class);
